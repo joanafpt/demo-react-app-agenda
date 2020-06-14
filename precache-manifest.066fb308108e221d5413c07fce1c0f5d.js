85362966ca8c4e5007ff43f05f7abac50648ac54
Joana Teixeira <joanajteixeira@outlook.com>
2020-06-14 21:38:19 +0100

    Updates

---
+++ precache-manifest.066fb308108e221d5413c07fce1c0f5d.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359638d638b5895107bd7c1f1922ddfe",
+    "url": "/demo-react-app-agenda/index.html"
+  },
+  {
+    "revision": "8c183a139b798680d74c",
+    "url": "/demo-react-app-agenda/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e17bf92b73e3b3eeba8e",
+    "url": "/demo-react-app-agenda/static/css/main.cd3ff552.chunk.css"
+  },
+  {
+    "revision": "8c183a139b798680d74c",
+    "url": "/demo-react-app-agenda/static/js/2.64218d53.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo-react-app-agenda/static/js/2.64218d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17bf92b73e3b3eeba8e",
+    "url": "/demo-react-app-agenda/static/js/main.4396b1c0.chunk.js"
+  },
+  {
+    "revision": "c0c5faccad74466392e5",
+    "url": "/demo-react-app-agenda/static/js/runtime-main.6520109b.js"
+  },
+  {
+    "revision": "b939dd73993298264270a906191b8e4a",
+    "url": "/demo-react-app-agenda/static/media/diario_1_ios.b939dd73.png"
+  },
+  {
+    "revision": "730544d81a94a62d4c4bbe7d14b6db42",
+    "url": "/demo-react-app-agenda/static/media/diario_2_ios.730544d8.png"
+  },
+  {
+    "revision": "42283c9a91d66343927842af003bb16f",
+    "url": "/demo-react-app-agenda/static/media/diario_3_ios.42283c9a.png"
+  },
+  {
+    "revision": "c661ec241a0b3d16f38fd0100c68b251",
+    "url": "/demo-react-app-agenda/static/media/diario_4_ios.c661ec24.png"
+  },
+  {
+    "revision": "4d7d6187990566f477f02e5ef54fbe08",
+    "url": "/demo-react-app-agenda/static/media/diario_android_1.4d7d6187.png"
+  },
+  {
+    "revision": "c0651ddcdbbffa11e6494bfd98f48270",
+    "url": "/demo-react-app-agenda/static/media/diario_android_2.c0651ddc.png"
+  },
+  {
+    "revision": "17e89792a94c3fc0f089d6d682b296ad",
+    "url": "/demo-react-app-agenda/static/media/diario_android_3.17e89792.png"
+  },
+  {
+    "revision": "469a9dba945e2820bdaf70665240503b",
+    "url": "/demo-react-app-agenda/static/media/diario_output_android.469a9dba.png"
+  },
+  {
+    "revision": "cb3e29be4fc52c344e149d6cfb149495",
+    "url": "/demo-react-app-agenda/static/media/diario_pf_aguarde_android.cb3e29be.png"
+  },
+  {
+    "revision": "7a7dc32903d0cdc32c6b504e1491eaa1",
+    "url": "/demo-react-app-agenda/static/media/ecra_inicial_android.7a7dc329.png"
+  },
+  {
+    "revision": "3e1eb2eb5379af431d46cc0d1d7987c8",
+    "url": "/demo-react-app-agenda/static/media/ecra_inicial_ios.3e1eb2eb.png"
+  },
+  {
+    "revision": "70f21f18873f0bb63cc06512bc3588c9",
+    "url": "/demo-react-app-agenda/static/media/menu_inicial_android.70f21f18.png"
+  },
+  {
+    "revision": "2b70db845e2bace217ebe2efd4be50b5",
+    "url": "/demo-react-app-agenda/static/media/menu_inicial_ios.2b70db84.png"
+  },
+  {
+    "revision": "e2d2912fd20170aef937b3744af925e1",
+    "url": "/demo-react-app-agenda/static/media/novo_post_android.e2d2912f.png"
+  },
+  {
+    "revision": "120eabd03950e394a93ce065353ade1f",
+    "url": "/demo-react-app-agenda/static/media/novo_post_android_2.120eabd0.png"
+  },
+  {
+    "revision": "dcdc6b20bf1ffc747227c78514598417",
+    "url": "/demo-react-app-agenda/static/media/novo_post_android_3.dcdc6b20.png"
+  },
+  {
+    "revision": "c92dd456de919fc288b2cc6d278568ca",
+    "url": "/demo-react-app-agenda/static/media/novo_post_android_4.c92dd456.png"
+  },
+  {
+    "revision": "a4b1279004343cc657daa044bea2d1a0",
+    "url": "/demo-react-app-agenda/static/media/novo_post_android_5.a4b12790.png"
+  },
+  {
+    "revision": "7e0d00e481241572d5b1195910832bf3",
+    "url": "/demo-react-app-agenda/static/media/novo_post_ios.7e0d00e4.png"
+  },
+  {
+    "revision": "74a2fa968d9437f50177500febdaff60",
+    "url": "/demo-react-app-agenda/static/media/novo_post_ios_2.74a2fa96.png"
+  },
+  {
+    "revision": "4d710970f247f4a7aab790325027c67d",
+    "url": "/demo-react-app-agenda/static/media/novo_post_ios_3.4d710970.png"
+  },
+  {
+    "revision": "41f772c61bc4a935c7a3d0b9c6239611",
+    "url": "/demo-react-app-agenda/static/media/novo_post_ios_4.41f772c6.png"
+  },
+  {
+    "revision": "3c75dcec42917850eb44c73ea31699e9",
+    "url": "/demo-react-app-agenda/static/media/novo_post_ios_5.3c75dcec.png"
+  },
+  {
+    "revision": "54d5975e68891bb1ec5333b7fd5190f9",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_1.54d5975e.png"
+  },
+  {
+    "revision": "ce2a935db44619635e4ff5a91b210b9c",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_2.ce2a935d.png"
+  },
+  {
+    "revision": "4f1e62322dbef71faaaed087f4f7cc72",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_3.4f1e6232.png"
+  },
+  {
+    "revision": "a592955897d65ae53e98e8b4eee01e53",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_5.a5929558.png"
+  },
+  {
+    "revision": "8d0476baa24637ac4abce7fb30aa7a35",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_NEW.8d0476ba.png"
+  },
+  {
+    "revision": "e5925fa52e813cb6f4763cb417ca36e9",
+    "url": "/demo-react-app-agenda/static/media/pesquisa_intervalo_tempo_android_NEW_DOIS.e5925fa5.png"
+  },
+  {
+    "revision": "c68d0ab6a919654133edae1967097405",
+    "url": "/demo-react-app-agenda/static/media/todos_compromissos_android_1.c68d0ab6.png"
+  },
+  {
+    "revision": "49c92d4706451438d466c7aac154fc8c",
+    "url": "/demo-react-app-agenda/static/media/todos_compromissos_android_2.49c92d47.png"
+  },
+  {
+    "revision": "d67dd866d2bb8c310c1091cefda1aace",
+    "url": "/demo-react-app-agenda/static/media/todos_ios.d67dd866.png"
+  },
+  {
+    "revision": "801b29cba22db6144dbf3518d630bee2",
+    "url": "/demo-react-app-agenda/static/media/todos_ios_2.801b29cb.png"
+  },
+  {
+    "revision": "356ca3e5dd8e6b72af0efe33697783e3",
+    "url": "/demo-react-app-agenda/static/media/tranche_ios_1.356ca3e5.png"
+  },
+  {
+    "revision": "d9de7fb157b46aa7823c3a234ef0b5dc",
+    "url": "/demo-react-app-agenda/static/media/tranche_ios_2.d9de7fb1.png"
+  },
+  {
+    "revision": "b89362ab0f572bc825724f6e98e6943e",
+    "url": "/demo-react-app-agenda/static/media/tranche_ios_3.b89362ab.png"
+  },
+  {
+    "revision": "0bdc62704be06937851dc94965ebc18d",
+    "url": "/demo-react-app-agenda/static/media/tranche_ios_4.0bdc6270.png"
+  },
+  {
+    "revision": "aea655ac8a2eea373e7b1f9422d372a3",
+    "url": "/demo-react-app-agenda/static/media/tranche_ios_5.aea655ac.png"
+  }
+]);
